a2b0783964feab4bfaea43709608bea28f63b0e1
Andrey999 <gromovandrey1985@gmail.com>
2021-06-06 20:29:43 +0300

    add login/errors

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { API_URL, API_KEY_3 } from '../../api/api'
 import { fetchApi } from '../../utils/fetchApi'
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 export const Login = () => {
-    const sendPromises = async () => {
+    const [open, setOpen] = useState(false)
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState({})
+
+    const onSubmit = async () => {
         try {
             // получение request token
             const getToken = await fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
@@ -17,8 +27,8 @@ export const Login = () => {
                     'content-type': 'application/json'
                 },
                 body: JSON.stringify({
-                    username: 'Andrey999',
-                    password: 'Gromovandrey@198',
+                    username: userName,
+                    password,
                     request_token: getToken.request_token
                 })
             })
@@ -33,13 +43,94 @@ export const Login = () => {
                 body: JSON.stringify({ request_token: getValidateWithLogin.request_token })
             })
         } catch (error) {
-            console.log('error: ', error)
+            setErrors(({ errors: { base: error.status_message } }))
         }
+    }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const validateFields = () => {
+        if (userName === '') {
+            setErrors((prev: any) => ({ ...prev, userName: 'not empty' }))
+        }
+        if (password === '') {
+            setErrors((prev: any) => ({ ...prev, password: 'not empty' }))
+        }
+        else {
+            setErrors({})
+        }
+    }
+
+    const onLogin = (event: any) => {
+        event.preventDefault()
+        if (Object.keys(errors).length > 0) {
+            validateFields()
+        } else {
+            onSubmit()
+        }
+    }
+
+    const changeUsername = (event: any) => {
+        setUserName(event.target.value)
+        setErrors((prev: any) => ({ ...prev, userName: null }))
+    }
+
+    const changePassword = (event: any) => {
+        setPassword(event.target.value)
+        setErrors((prev: any) => ({ ...prev, password: null }))
     }
 
     return (
         <div>
-            <Button color="inherit" onClick={sendPromises}>Login</Button>
+            <Button color="inherit" onClick={handleClickOpen}>Login</Button>
+            <div>
+                <Dialog open={open} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Авторизация</DialogTitle>
+                    <div>{errors.base}</div>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="username"
+                            label="Логин"
+                            type="text"
+                            fullWidth
+                            name="username"
+                            value={userName}
+                            onChange={changeUsername}
+                        />
+                        {errors.userName && (<div>{errors.userName}</div>)}
+                        <TextField
+                            margin="dense"
+                            id="password"
+                            label="Пароль"
+                            type="password"
+                            fullWidth
+                            name="password"
+                            value={password}
+                            onChange={changePassword}
+                        />
+                        {errors.password && (<div>{errors.password}</div>)}
+
+                    </DialogContent>
+
+                    <DialogActions>
+                        <Button onClick={onLogin} color="primary">
+                            Вход
+                        </Button>
+                    </DialogActions>
+
+                    <DialogActions>
+                        {errors.base && (<div>{errors.base}</div>)}
+                    </DialogActions>
+                </Dialog>
+            </div>
         </div>
     );
 }
